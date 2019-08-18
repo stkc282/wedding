a798fdfe6f6f1014ba364f3c1d11e703aa590f9a
Your Name <you@example.com>
2019-08-18 12:50:39 +0900

    first

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/takashi/PycharmProjects/Django-Simple-CRUD-Sample-master/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
