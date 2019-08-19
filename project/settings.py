4b1efc2a3760509a5deab732bc2871ac443f9192
Your Name <you@example.com>
2019-08-19 16:13:58 +0900

    make it better

--- project/settings.py
+++ project/settings.py
@@ -24,16 +24,16 @@ SECRET_KEY = '4q8nhf65e@0u3meznwy6-ctgy8p+_9)3!1*j49smp5y+m5fx2m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-SESION_SECURE = True
+# SESION_SECURE = True
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
+#
+# if not DEBUG:
+#     import django_heroku
+#     django_heroku.settings(locals())
 
 ALLOWED_HOSTS = ["*"]
 
